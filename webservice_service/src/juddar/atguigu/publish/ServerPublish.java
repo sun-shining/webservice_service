677bfee9d6e6d2826ec752f3177947026f567175
juddar <lnhjds@126.com>
2015-07-31 13:19:27 +0800

    update 08

--- webservice_service/src/juddar/atguigu/publish/ServerPublish.java
+++ webservice_service/src/juddar/atguigu/publish/ServerPublish.java
@@ -7,7 +7,7 @@ import juddar.atguigu.service.HelloWsImpl;
 public class ServerPublish {
 	public static void main(String[] args) {
 		
-		System.out.println("-----------------------------");
+		System.out.println("----!!!!!!!-------------------------");
 		String address = "http://192.168.10.5:8989/juddar/sayHello";
 		Endpoint.publish(address , new HelloWsImpl());
 		
